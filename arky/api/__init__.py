22e4b08392ae02f18d0873ae13964b87e28f6a4a
Moustikitos <bruno.thoorens@free.fr>
2017-01-08 21:33:22 +0100

    0.0.2
    arky.api security fix

--- arky/api/__init__.py
+++ arky/api/__init__.py
@@ -1,151 +1,123 @@
 # -*- encoding: utf8 -*-
 # created by Toons on 01/05/2017
+# only GET method is implemented, no POST or PUT for security reasons
 
 import json, requests
 
 __URL_BASE__ = "http://node1.arknet.cloud:4000"
 
-
-def get(api, dic={}, **kw):
-	return json.loads(requests.get(__URL_BASE__+api, params=dict(dic, **kw)).text)
-
-def post(api, dic={}, **kw):
-	return json.loads(requests.post(__URL_BASE__+api, params=dict(dic, **kw)).text)
-
-def put(api, dic={}, **kw):
-	return json.loads(requests.put(__URL_BASE__+api, params=dict(dic, **kw)).text)
+def get(api, **kw):
+	return json.loads(requests.get(__URL_BASE__+api, params=dic).text)
 
 
 class Loader:
 
-	def getLoadingStatus(self):
+	@staticmethod
+	def getLoadingStatus():
 		return get('/api/loader/status')
 
-	def getSynchronisationStatus(self):
+	@staticmethod
+	def getSynchronisationStatus():
 		return get('/api/loader/status/sync')
 
 
 class Block:
 
-	def getBlock(self, blockId):
+	@staticmethod
+	def getBlock(blockId):
 		return get('/api/blocks/get', id=blockId)
 
-	def getBlocks(self):
+	@staticmethod
+	def getBlocks():
 		return get('/api/blocks')
 
-	def getBlockchainFee(self):
+	@staticmethod
+	def getBlockchainFee():
 		return get('/api/blocks/getFee')
 
-	def getBlockchainHeight(self):
+	@staticmethod
+	def getBlockchainHeight():
 		return get('/api/blocks/getHeight')
 
-	def getForgedByAccount(self, publicKey):
+	@staticmethod
+	def getForgedByAccount(publicKey):
 		return get('/api/delegates/forging/getForgedByAccount', generatorPublicKey=publicKey)
 
 
 class Account:
 
-	def openAccount(self, secret):
-		return post('/api/accounts/open', secret=secret)
-
-	def generatePublicKey(self, secret):
-		return post('/api/accounts/generatePublicKey', secret=secret)
-
-	def getBalance(self, address):
+	@staticmethod
+	def getBalance(address):
 		return get('/api/accounts/getBalance', address=address)
 
-	def getPublicKey(self, address):
+	@staticmethod
+	def getPublicKey(address):
 		return get('/api/accounts/getPublicKey', address=address)
 
-	def getAccount(self, address):
+	@staticmethod
+	def getAccount(address):
 		return get('/api/accounts', address=address)
 
-	def getVotes(self, address):
+	@staticmethod
+	def getVotes(address):
 		return get('/api/accounts/delegates', address=address)
-
-	def vote(self, secret, *delegates, secondSecret=None, publicKey=None):
-		param = {"secret":secret, "delegates":delegates}
-		if secondSecret: param["secondSecret"] = secondSecret
-		if publicKey: param["publicKey"] = publicKey
-		return put('/api/accounts/delegates', **param)
-
-	def addSecondSignature(secret, secondSecret):
-		return put('/api/signatures', secret=secret, secondSecret=secondSecret)
 
 
 class Delegate:
 
-	def enableDelegateOnAccount(self, secret, username, secondSecret=None):
-		param = {"secret":secret, "username":username}
-		if secondSecret: param["secondSecret"] = secondSecret
-		return put('/api/delegates', **param)
+	@staticmethod
+	def getDelegates():
+		return get('/api/delegates')
 
-	def getDelegates(self):
-		return get('/api/delegates', **param)
-
-	def getDelegate(self, username):
+	@staticmethod
+	def getDelegate(username):
 		return get('/api/delegates/get', username=username)
 
-	def getVoters(self, publicKey):
+	@staticmethod
+	def getVoters(publicKey):
 		return get('/api/delegates/voters', publicKey=publicKey)
-
-	def enableForging(self, secret):
-		return post('/api/delegates/forging/enable', secret=secret)
-
-	def disableForging(self, secret):
-		return post('/api/delegates/forging/disable', secret=secret)
 
 
 class Transaction:
 
-	def getTransactionsList(self):
-		return get('/api/transactions', publicKey=publicKey)
+	@staticmethod
+	def getTransactionsList():
+		return get('/api/transactions')
 
-	def sendTransaction(self, secret, amount, recipientId, publicKey=None, secondSecret=None):
-		param = {"secret":secret, "amount":amount, "recipientId":recipientId}
-		if secondSecret: param["secondSecret"] = secondSecret
-		if publicKey: param["publicKey"] = publicKey
-		return put('/api/transactions', **param)
-
-	def getTransaction(self, transactionId):
+	@staticmethod
+	def getTransaction(transactionId):
 		return get('/api/transactions/get', id=transactionId)
 
-	def getUnconfirmedTransaction(self, transactionId):
+	@staticmethod
+	def getUnconfirmedTransaction(transactionId):
 		return get('/api/transactions/unconfirmed/get', id=transactionId)
 
-	def getUnconfirmedTransactions(self):
+	@staticmethod
+	def getUnconfirmedTransactions():
 		return get('/api/transactions/unconfirmed')
 
 
 class Peer:
 
-	def getPeersList(self):
+	@staticmethod
+	def getPeersList():
 		return get('/api/peers')
 
-	def getPeers(self, ip, port):
+	@staticmethod
+	def getPeers(ip, port):
 		return get('/api/peers', ip=ip, port=port)
 
-	def getPeerVersion(self):
+	@staticmethod
+	def getPeerVersion():
 		return get('/api/peers/version')
 
 
 class Multisignature:
 
-	def getPendingMultiSignatureTransactions(self, publicKey):
+	@staticmethod
+	def getPendingMultiSignatureTransactions(publicKey):
 		return get('/api/multisignatures/pending', publicKey=publicKey)
 
-	def createMultiSignatureAccount(self, secret, lifetime, minimum, keysgroup):
-		return put('/api/multisignatures',
-			secret=secret,
-			lifetime=lifetime,
-			min=minimum,
-			keysgroup=keysgroup
-		)
-
-	def signTransaction(self, secret, transactionId, publicKey=None):
-		param = {"secret":secret, "transactionId":transactionId}
-		if publicKey: param["publicKey"] = publicKey
-		return post('/api/multisignatures/sign', **param)
-
-	def getAccountsOfMultisignature(self, publicKey):
+	@staticmethod
+	def getAccountsOfMultisignature(publicKey):
 		return post('/api/multisignatures/accounts', publicKey=publicKey)
---
+++ arky/core.py
@@ -0,0 +1,144 @@
+# -*- encoding: utf8 -*-
+from . import StringIO, __PY3__, __FEES__, slots, ArkObject
+
+from nacl.bindings.crypto_sign import crypto_sign_seed_keypair, crypto_sign
+import struct, base58, hashlib
+# array,
+
+unpack =     lambda fmt, fileobj: struct.unpack(fmt, fileobj.read(struct.calcsize(fmt)))
+pack =       lambda fmt, fileobj, value: fileobj.write(struct.pack(fmt, *value))
+pack_bytes = lambda f,v: pack("!"+"%ss"%len(v), f, (v,)) if __PY3__ else \
+             lambda f,v: pack("!"+"c"*len(v), f, v)
+
+# def getKeys(secret):
+# 	keys = ArkObject()
+# 	seed = hashlib.sha256(secret.encode() if not isinstance(secret, bytes) else secret).digest()
+# 	keys.public, keys.private = crypto_sign_seed_keypair(seed)
+# 	return keys
+
+def getBytes(transaction):
+	buf = StringIO()
+
+	pack("!b", buf, (transaction.type,))
+	pack("!i", buf, (transaction.timestamp,))
+	pack_bytes(buf, transaction.senderPublicKey)
+
+	if hasattr(transaction, "requesterPublicKey"):
+		pack_bytes(buf, transaction.requesterPublicKey)
+
+	if hasattr(transaction, "recipientId"):
+		recipientId = base58.b58decode_check(transaction.recipientId[:-1])
+	else:
+		recipientId = b"\x00"*32
+	pack_bytes(buf,recipientId)
+
+	if hasattr(transaction, "vendorField"):
+		n = len(transaction.vendorField)
+		vendorField = transaction.vendorField + b"\x00"*(64-n)
+	else:
+		vendorField = b"\x00"*64
+	pack_bytes(buf, vendorField)
+
+	pack("!l", buf, (transaction.amount,))
+
+	typ  = transaction.type
+	if typ == 1: # signature
+		pack_bytes(buf, transaction.asset.signature)
+	elif typ == 2: # delegate
+		pack_bytes(buf, transaction.asset.delegate.username)
+	elif typ == 3 and hasattr(transaction.asset, "vote"): # vote
+		pack_bytes(buf, b"".join(transaction.asset.vote))
+	elif typ == 4: # multi sigature
+		pack("!b", buf, (transaction.asset.multisignature.min,))
+		pack("!b", buf, (transaction.asset.multisignature.lifetime,))
+		pack_bytes(buf, b"".join(transaction.asset.multisignature.keysgroup))
+
+	if hasattr(transaction, "signature"):
+		pack_bytes(buf, transaction.signature)
+	
+	if hasattr(transaction, "signSignature"):
+		pack_bytes(buf, transaction.signSignature)
+
+	result = buf.getvalue()
+	buf.close()
+	return result.encode() if not isinstance(result, bytes) else result
+
+
+class Transaction(ArkObject):
+
+	# adress = property(lambda obj: base58.b58encode_check(obj.senderPublicKey) + "A", None, None, "")
+
+	def __init__(self, **kwargs):
+		self.type = kwargs.get("type", 1)
+		self.amount = kwargs.get("amount", 0)
+		self.timestamp = slots.getTime()
+		self.asset =  ArkObject()
+
+	def __setattr__(self, attr, value):
+		if hasattr(self, "_bytes"): delattr(self, "_bytes")
+		if hasattr(self, "_hash"): delattr(self, "_hash")
+
+		if attr == "secret":
+			keys = getKeys(value)
+			ArkObject.__setattr__(self, "senderPublicKey", keys.public)
+			self.sign(keys.private)
+		elif attr == "secondSecret":
+			self.secondSign(getKeys(value).private)
+		elif attr == "type":
+			if value == 1: self.fee = __FEES__.secondsignature
+			elif value == 2: self.fee = __FEES__.delegate
+			elif value == 3: self.fee = __FEES__.vote
+			elif value == 4: self.fee = __FEES__.multisignature
+			ArkObject.__setattr__(self, attr, value)
+		else:
+			ArkObject.__setattr__(self, attr, value)
+
+	def getBytes(self):
+		if not hasattr(self, "_bytes"):
+			ArkObject.__setattr__(self, "_bytes", getBytes(self))
+		return getattr(self, "_bytes")
+
+	def getId(self):
+		h = hashlib.sha256(self.getBytes()).hexdigest()[-8:]
+		return int("0x"+h[-1:0:-1]+h[0:1], base=16)
+
+	def check(self):
+		pass
+
+	def doubleCheck(self):
+		pass
+
+	# def getHash(self):
+	# 	if not hasattr(self, "_hash"):
+	# 		ArkObject.__setattr__(self, "_hash", hashlib.sha256(self.getBytes()).digest())
+	# 	return getattr(self, "_hash")
+
+	# def sign(self, private):
+	# 	ArkObject.__setattr__(self, "signature", crypto_sign(self.getHash(), private))
+
+	# def secondSign(self, private):
+	# 	ArkObject.__setattr__(self, "signSignature", crypto_sign(self.getHash(), private))
+
+# function sign(transaction, keys) {
+# 	var hash = getHash(transaction);
+
+# 	var signature = keys.sign(hash).toDER().toString("hex");
+
+# 	if (!transaction.signature) {
+# 		transaction.signature = signature;
+# 	}
+# 	return signature;
+
+# }
+
+# function secondSign(transaction, keys) {
+# 	var hash = getHash(transaction);
+
+# 	var signature = keys.sign(hash).toDER().toString("hex");
+
+# 	if (!transaction.signSignature) {
+# 		transaction.signSignature = signature;
+# 	}
+# 	return signature;
+# }
+
